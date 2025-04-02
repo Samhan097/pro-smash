5446988c18cdf36306028d1c01d67668091b730a
Samhan097 <48414384+Samhan097@users.noreply.github.com>
2025-04-02 14:44:51 +0530

    modified

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 import express from "express";
-import cookieParser from "cookie-parser";
+// import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import cors from "cors";
-// import { connectDB } from "./config/db.js";
-import connectDB from "./config/db.js";
-import authRouter from "./routes/authRoutes.js";
+// import cors from "cors";
+// import connectDB from "./config/db.js";
+// import authRouter from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
-connectDB();
+// connectDB();
 
-app.use(express.json()); 
-app.use(cookieParser()); 
-app.use(cors({ credentials: true }));
+// app.use(express.json()); 
+// app.use(cookieParser()); 
+// app.use(cors({ credentials: true }));
 
 // API EndPoints
 app.get("/", (req, res) => {
-  res.send("API Working");
+  res.status(200).json({msg:"API Working"});
 });
-app.use('/api/auth',authRouter)
+// app.use('/api/auth',authRouter)
 
-app.listen(port,'0.0.0.0', () => {
+app.listen(port, () => {
   // connectDB()
   console.log(`Server Started on PORT http://localhost:${port}`);
 });
