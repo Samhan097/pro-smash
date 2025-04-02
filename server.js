f70f5a03f3b8e2495f6a7796971b59f8939b353b
Samhan097 <48414384+Samhan097@users.noreply.github.com>
2025-04-02 18:52:19 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 import express from "express";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-// import cors from "cors";
-// import connectDB from "./config/db.js";
-// import authRouter from "./routes/authRoutes.js";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 8080;
-// connectDB();
+const port = process.env.PORT || 4000;
+connectDB();
 
-// app.use(express.json()); 
-// app.use(cookieParser()); 
-// app.use(cors({ credentials: true }));
+app.use(express.json()); 
+app.use(cookieParser()); 
+app.use(cors({ credentials: true }));
 
 // API EndPoints
 app.get("/", (req, res) => {
-  res.status(200).json({msg:"API Working"});
+  res.send("API Working");
 });
-// app.use('/api/auth',authRouter)
+app.use('/api/auth',authRouter)
 
-app.listen(port, '0.0.0.0',() => {
-  // connectDB()
-  console.log(`Server Started on PORT http://localhost:${port}`); 
+app.listen(port, () => {
+  console.log(`Server Started on PORT http://localhost:${port}`);
 });
 // OwHH5YTCzLCa8cQj
